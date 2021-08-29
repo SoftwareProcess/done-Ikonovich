645783ce21a630db2f2b94c2a135497ad141fa9d
Evan <evanhnly@gmail.com>
2021-08-29 15:54:51 -0500

    Initial commit. Collapse.py written with unit tests

--- Assignment/collapse.py
+++ Assignment/collapse.py
@@ -1,2 +1,27 @@
+# Evan Henley
+# EAH0102
+# August 29, 2021
+
+# This method collapses any non-negative integer value, provided as a string,
+# into a single digit.
+
 def collapse(value):
-    ...
+    
+    if (isinstance(value,str) and (value.isnumeric()) and (len(value) < 51)):
+        
+        storedVal = value
+        
+        while (len(storedVal) > 1):
+            tempVal = 0
+            
+            for character in storedVal: 
+            
+                tempVal += int(character)
+            
+            storedVal = str(tempVal)
+            
+            
+        return storedVal
+    
+    else:
+        return None
--- Assignment/collapseTest.py
+++ Assignment/collapseTest.py
@@ -11,3 +11,58 @@ class CollapseTest(unittest.TestCase):
         actualResult = collapse.collapse(value)
         self.assertEqual(expectedResult, actualResult)
     
+    def test_ShouldCollapseFiveDigits(self):
+        value = '11121'
+        expectedResult = '6'
+        actualResult = collapse.collapse(value)
+        self.assertEqual(expectedResult, actualResult)
+        
+            
+    def test_ShouldCollapseMultipleDigitInitialSum(self):
+        value = '123456789'
+        expectedResult = '9'
+        actualResult = collapse.collapse(value)
+        self.assertEqual(expectedResult, actualResult)
+        
+    def test_ShouldCollapseFiftyDigits(self):
+        value = '15948305706294801849230528214092429723429482193121'
+        expectedResult = '6'
+        actualResult = collapse.collapse(value)
+        self.assertEqual(expectedResult, actualResult)
+        
+                    
+    def test_ShouldCollapseAllZeroes(self):
+        value = '000000'
+        expectedResult = '0'
+        actualResult = collapse.collapse(value)
+        self.assertEqual(expectedResult, actualResult)
+        
+    def test_ShouldNotCollapseNonNumeric(self):
+        value = 'abc'
+        expectedResult = None
+        actualResult = collapse.collapse(value)
+        self.assertEqual(expectedResult, actualResult)
+        
+    def test_ShouldNotCollapseFiftyOneDigits(self):
+        value = '159483057062948018492305282140924297234294821931215'
+        expectedResult = None
+        actualResult = collapse.collapse(value)
+        self.assertEqual(expectedResult, actualResult)
+        
+    def test_ShouldNotCollapseZeroDigits(self):
+        value = ''
+        expectedResult = None
+        actualResult = collapse.collapse(value)
+        self.assertEqual(expectedResult, actualResult)
+        
+    def test_ShouldNotCollapseValueNone(self):
+        value = None
+        expectedResult = None
+        actualResult = collapse.collapse(value)
+        self.assertEqual(expectedResult, actualResult)
+        
+    def test_ShouldNotCollapseValueBelowZero(self):
+        value = -5
+        expectedResult = None
+        actualResult = collapse.collapse(value)
+        self.assertEqual(expectedResult, actualResult)
